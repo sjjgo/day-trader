584dcf776fe111424ecc8286fa3833bf3b70ccac
ryantantiern <ryantantiern@gmail.com>
2017-04-17 00:25:25 +0100

    again

--- server.js
+++ server.js
@@ -49,16 +49,15 @@ function handleError(res, reason, message, code) {
 
 app.post("/api/game-codes/validate", function(req, res) {
 	if (!req.body.gamecode) {
-		res.status(400).json(req.body);
 		handleError(res, "Invalid game code", "Must provide a game code", 400);
 	}
 
-	db.collection(GAMECODES_COLLECTION).find({game_code : req.body.gamecode).toArray(function(err, doc){
+	db.collection(GAMECODES_COLLECTION).find({game_code : req.body.gamecode).toArray(function(err, docs){
 		if (err) {
 			handleError(res, err.message, "Failed to retrieve game code", 500);
 		}
 		else {
-			res.status(201).json(doc.ops[0]);
+			res.status(201).json(docs);
 		}
 	});
 });
