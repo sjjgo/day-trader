e9735b90a7953cd38fe599153f7c1a8740456a7d
ryantantiern <ryantantiern@gmail.com>
2017-06-03 13:14:13 +0100

    typo

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,4 +1,4 @@
-export const hostname = "https://pacific-stream-42154.herokuapp.com/";
-//https://pacific-stream-42154.herokuapp.com/
+export const hostname = "https://pacific-stream-42154.herokuapp.com";
+//https://pacific-stream-42154.herokuapp.com
 //http://localhost:8080
 export const number_of_players = 4;
